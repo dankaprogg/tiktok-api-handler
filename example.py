5c18ed122795064222a75d0b3afb3e554c116527
flyingwr <renanferncmrc@gmail.com>
2022-03-21 02:58:30 -0300

    first commit

---
+++ example.py
@@ -0,0 +1,17 @@
+from tiktok import TikTok
+
+import asyncio
+
+async def main():
+    client = TikTok()
+    user = await client.get_user("@renankkkjk")
+    if user:
+        print(f"User nickname: {user.nickname}")
+    else:
+        print("User not found")
+
+    await client.close()
+    await asyncio.sleep(1.0)
+
+if __name__ == "__main__":
+    asyncio.run(main())
